c31163a40991d54345c32e48f4b574f0789c2c97
Farazjahangir <jahangirfaraz98@gmail.com>
2024-10-10 14:54:59 +0500

    added back icon in header and header code refactored

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,33 +1,28 @@
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 
 import {logout} from '../../Redux/user/userSlice';
+import Button from '../Button';
+import backIcon from '../../Assets/icons/back.png';
+import styles from './style';
 
-const Header = ({route}) => {
+const Header = ({route, navigation}) => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.user.data.user)
+  const user = useSelector(state => state.user.data.user);
   return (
-    <View
-      style={{
-        backgroundColor: '#ffffff',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 14,
-        alignItems: 'center'
-      }}>
-      <Text style={{color: 'black', fontSize: 20}}>{route.name}</Text>
-      <Text style={{color: 'black', fontSize: 16}}>Hi, {user.name}</Text>
-      <TouchableOpacity
-        style={{
-          backgroundColor: '#ff4d4d',
-          width: 60,
-          borderRadius: 5,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        onPress={() => dispatch(logout())}>
-        <Text style={{color: '#ffffff'}}>Logout</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <View style={styles.leftSide}>
+        {navigation.canGoBack() && (
+          <TouchableOpacity
+            style={styles.backIconBox}
+            onPress={() => navigation.goBack()}>
+            <Image source={backIcon} style={styles.backIcon} />
+          </TouchableOpacity>
+        )}
+        <Text style={styles.screenName}>{route.name}</Text>
+      </View>
+      <Text style={styles.username}>Hi, {user.name}</Text>
+      <Button text="Logout" onPress={() => dispatch(logout())} />
     </View>
   );
 };
---
+++ src/Components/Header/style.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import { RFPercentage } from 'react-native-responsive-fontsize';
+import COLORS from '../../Constants/colors';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: COLORS.white,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 14,
+        alignItems: 'center',
+    },
+    screenName: {
+        color: COLORS.black, 
+        fontSize: RFPercentage(3)
+    },
+    username: {
+        color: COLORS.black, 
+        fontSize: RFPercentage(2.5)
+    },
+    leftSide: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    backIconBox: {
+        width: 20,
+        height: 20,
+        marginRight: 10
+    },
+    backIcon: {
+        width: '100%',
+        height: '100%'
+    }
+});
+
+export default styles;
