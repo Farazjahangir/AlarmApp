b88ac707ce39e066ed8ad7c7f26e8813c368e83f
Farazjahangir <jahangirfaraz98@gmail.com>
2024-11-26 17:34:05 +0500

    removed firebase config files

--- android/app/src/main/java/com/forteWiz/ResQ/MainActivity.kt
+++ android/app/src/main/java/com/forteWiz/ResQ/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.forteWiz.ResQ
+package com.fortewiz.resq
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/forteWiz/ResQ/MainApplication.kt
+++ android/app/src/main/java/com/forteWiz/ResQ/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.forteWiz.ResQ
+package com.fortewiz.resq
 
 import android.app.Application
 import com.facebook.react.PackageList
